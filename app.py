81a87b8c21556c07cc182184e5768ecc562fbba3
Madhusudhan B <madhuhotmail2005@gmail.com>
2022-09-23 18:05:34 +0530

    Sample commit for testing

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ scalar=pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
+# @app.route('/predict_api',methods=['POST'])
 # def predict_api():
 #     data=request.json['data']
 #     print(data)
